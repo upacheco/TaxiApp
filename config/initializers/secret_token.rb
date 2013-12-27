a4b1dae1d3de7a88e42108b3fb9d7eefa5f9f4e4
Ulises <upacheco@pernix-solutions.com>
2013-12-26 20:26:22 -0600

    Basic Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaxiApp::Application.config.secret_key_base = '05b19d96283f14697b466ca46d43d3fa3b30a3776ede5d606d345a40be90a418b48b0a2c095c978c6517ed07457f361f4de33607c6ece77a6ddc354444bdf3f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaxiApp::Application.config.session_store :cookie_store, key: '_taxi_app_session'
